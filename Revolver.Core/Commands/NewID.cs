7a02ea3225cf312d967ce4d98aa1120d8416e741
Alistair Deneys <ad@codeflood.net>
2017-08-12 22:09:53 +1000

    Added New ID command

---
+++ Revolver.Core/Commands/NewID.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sitecore.Data;
+
+namespace Revolver.Core.Commands
+{
+  [Command("newid")]
+  public class NewID : BaseCommand
+  {
+    public override CommandResult Run()
+    {
+      return new CommandResult(CommandStatus.Success, ID.NewID.ToString());
+    }
+
+    public override string Description()
+    {
+      return "Generate a new ID";
+    }
+
+    public override void Help(HelpDetails details)
+    {
+    }
+  }
+}
