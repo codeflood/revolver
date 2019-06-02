e208f57b64e974befdf02800a187fb5a088ccc71
Alistair Deneys <ad@codeflood.net>
2019-06-02 23:02:39 +1000

    Generate ISO format dates with option to make them human friendly

--- Revolver.Core/Commands/Random.cs
+++ Revolver.Core/Commands/Random.cs
@@ -1,147 +1,162 @@
-﻿using Sitecore.StringExtensions;
+﻿using Sitecore;
+using Sitecore.StringExtensions;
 using System;
 
 namespace Revolver.Core.Commands
 {
-    [Command("random")]
-    public class Random : BaseCommand
+  [Command("random")]
+  public class Random : BaseCommand
+  {
+    private readonly static System.Random RandomProvider = new System.Random();
+    private readonly static object RandomLock = new object();
+
+    [NumberedParameter(0, "max")]
+    [Description("The maximum allowed value.")]
+    [Optional]
+    public string Maximum { get; set; }
+
+    [NumberedParameter(1, "min")]
+    [Description("The minimum allowed value.")]
+    [Optional]
+    public string Minimum { get; set; }
+
+    [NamedParameter("f", "fractions")]
+    [Description("The number of fractal digits.")]
+    [Optional]
+    public int FractalDigits { get; set; }
+
+    [FlagParameter("d")]
+    [Description("Generate dates")]
+    [Optional]
+    public bool GenerateDates { get; set; }
+
+    [FlagParameter("t")]
+    [Description("Generate times")]
+    [Optional]
+    public bool GenerateTimes { get; set; }
+
+    [FlagParameter("h")]
+    [Description("Format the date as human friendly")]
+    [Optional]
+    public bool DateTimeFormatHuman { get; set; }
+
+    public Random()
     {
-        private readonly static System.Random RandomProvider = new System.Random();
-        private readonly static object RandomLock = new object();
-
-        [NumberedParameter(0, "max")]
-        [Description("The maximum allowed value.")]
-        [Optional]
-        public string Maximum { get; set; }
-
-        [NumberedParameter(1, "min")]
-        [Description("The minimum allowed value.")]
-        [Optional]
-        public string Minimum { get; set; }
-
-        [NamedParameter("f", "fractions")]
-        [Description("The number of fractal digits.")]
-        [Optional]
-        public int FractalDigits { get; set; }
-
-        [FlagParameter("d")]
-        [Description("Generate dates")]
-        [Optional]
-        public bool GenerateDates { get; set; }
-
-        [FlagParameter("t")]
-        [Description("Generate times")]
-        [Optional]
-        public bool GenerateTimes { get; set; }
-
-        public Random()
-        {
-            Maximum = string.Empty;
-            Minimum = string.Empty;
-            FractalDigits = 0;
-            GenerateDates = false;
-            GenerateTimes = false;
-        }
-
-        public override CommandResult Run()
-        {
-            if (FractalDigits != 0 && (GenerateDates || GenerateTimes))
-                return new CommandResult(CommandStatus.Failure, "Cannot use -d or -t with -f");
-
-            if (GenerateDates || GenerateTimes)
-                return RunDateTime();
-
-            return RunNumber();
-        }
-
-        protected virtual CommandResult RunNumber()
-        {
-            var parsedMinimum = 0;
-            var parsedMaximum = 10;
-
-            if (!string.IsNullOrEmpty(Minimum) && !int.TryParse(Minimum, out parsedMinimum))
-                return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as integer for parameter 'min'".FormatWith(Minimum));
-
-            if (!string.IsNullOrEmpty(Maximum) && !int.TryParse(Maximum, out parsedMaximum))
-                return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as integer for parameter 'max'".FormatWith(Minimum));
-
-            if (FractalDigits == 0)
-            {
-                // Random will be an integer as interval is integer
-                int value = GetRandomInteger(parsedMinimum, parsedMaximum);
-                return new CommandResult(CommandStatus.Success, value.ToString());
-            }
-            
-            // Random will be a double as interval is double
-            var rnum = GetRandomDouble();
-            var num = Math.Round(parsedMinimum + ((parsedMaximum - parsedMinimum) * rnum), FractalDigits);
-
-            string formatString = "F" + FractalDigits;
-            //F# = Force fractional digits, no commas. See http://msdn.microsoft.com/en-us/library/kfsatb94(v=vs.110).aspx
-            return new CommandResult(CommandStatus.Success, num.ToString(formatString));
-        }
-
-        protected virtual CommandResult RunDateTime()
-        {
-            var parsedMinimum = DateTime.UtcNow;
-            var parsedMaximum = parsedMinimum.AddMonths(6);
-
-            if (!string.IsNullOrEmpty(Minimum) && !DateTime.TryParse(Minimum, out parsedMinimum))
-                return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as datetime for parameter 'min'".FormatWith(Minimum));
-
-            if (!string.IsNullOrEmpty(Maximum) && !DateTime.TryParse(Maximum, out parsedMaximum))
-                return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as datetime for parameter 'max'".FormatWith(Minimum));
-
-            var value = parsedMinimum;
-
-            if (GenerateDates)
-            {
-                var addDays = GetRandomInteger(0, (int)Math.Ceiling((parsedMaximum - parsedMinimum).TotalDays));
-                value = value.AddDays(GetRandomInteger(0, addDays));
-            }
-
-            if (GenerateTimes)
-            {
-                var minuteSpan = (int)Math.Ceiling(parsedMaximum.TimeOfDay.TotalMinutes);
-                if (minuteSpan == 0)
-                    minuteSpan = (int) Math.Ceiling(new TimeSpan(24, 0, 0).TotalMinutes);
-
-                value = value.AddMinutes(GetRandomInteger(0, minuteSpan));
-            }
-
-            return new CommandResult(CommandStatus.Success, value.ToString());
-        }
-
-        protected int GetRandomInteger(int min, int max)
-        {
-            lock (RandomLock)
-            {
-                return RandomProvider.Next(min, max);
-            }
-        }
-
-        protected double GetRandomDouble()
-        {
-            lock (RandomLock)
-            {
-                return RandomProvider.NextDouble();
-            }
-        }
-
-        public override string Description()
-        {
-            return "Generate random numbers, dates and times";
-        }
-
-        public override void Help(HelpDetails details)
-        {
-            details.AddExample("10");
-            details.AddExample("5 50");
-            details.AddExample("10 -f 2");
-            details.AddExample("-d 2020-12-31");
-            details.AddExample("-d -t 2020-12-31");
-            details.AddExample("-t");
-            details.AddExample("-d -t 2010-01-01 2020-12-31");
-        }
+      Maximum = string.Empty;
+      Minimum = string.Empty;
+      FractalDigits = 0;
+      GenerateDates = false;
+      GenerateTimes = false;
+      DateTimeFormatHuman = false;
     }
+
+    public override CommandResult Run()
+    {
+      if (FractalDigits != 0 && (GenerateDates || GenerateTimes))
+        return new CommandResult(CommandStatus.Failure, "Cannot use -d or -t with -f");
+
+      if(DateTimeFormatHuman && (!GenerateDates && !GenerateTimes))
+        return new CommandResult(CommandStatus.Failure, "Parameter -h can only be used with -d or -t");
+
+      if (GenerateDates || GenerateTimes)
+        return RunDateTime();
+
+      return RunNumber();
+    }
+
+    protected virtual CommandResult RunNumber()
+    {
+      var parsedMinimum = 0;
+      var parsedMaximum = 10;
+
+      if (!string.IsNullOrEmpty(Minimum) && !int.TryParse(Minimum, out parsedMinimum))
+        return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as integer for parameter 'min'".FormatWith(Minimum));
+
+      if (!string.IsNullOrEmpty(Maximum) && !int.TryParse(Maximum, out parsedMaximum))
+        return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as integer for parameter 'max'".FormatWith(Minimum));
+
+      if (FractalDigits == 0)
+      {
+        // Random will be an integer as interval is integer
+        int value = GetRandomInteger(parsedMinimum, parsedMaximum);
+        return new CommandResult(CommandStatus.Success, value.ToString());
+      }
+
+      // Random will be a double as interval is double
+      var rnum = GetRandomDouble();
+      var num = Math.Round(parsedMinimum + ((parsedMaximum - parsedMinimum) * rnum), FractalDigits);
+
+      string formatString = "F" + FractalDigits;
+      //F# = Force fractional digits, no commas. See http://msdn.microsoft.com/en-us/library/kfsatb94(v=vs.110).aspx
+      return new CommandResult(CommandStatus.Success, num.ToString(formatString));
+    }
+
+    protected virtual CommandResult RunDateTime()
+    {
+      var parsedMinimum = DateTime.UtcNow.Date;
+      var parsedMaximum = parsedMinimum.AddMonths(6);
+
+      if (!string.IsNullOrEmpty(Minimum) && !DateTime.TryParse(Minimum, out parsedMinimum))
+        return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as datetime for parameter 'min'".FormatWith(Minimum));
+
+      if (!string.IsNullOrEmpty(Maximum) && !DateTime.TryParse(Maximum, out parsedMaximum))
+        return new CommandResult(CommandStatus.Failure, "Cannot parse '{0}' as datetime for parameter 'max'".FormatWith(Minimum));
+
+      var value = parsedMinimum;
+
+      if (GenerateDates)
+      {
+        var addDays = GetRandomInteger(0, (int)Math.Ceiling((parsedMaximum - parsedMinimum).TotalDays));
+        value = value.AddDays(GetRandomInteger(0, addDays));
+      }
+
+      if (GenerateTimes)
+      {
+        var minuteSpan = (int)Math.Ceiling(parsedMaximum.TimeOfDay.TotalMinutes);
+        if (minuteSpan == 0)
+          minuteSpan = (int)Math.Ceiling(new TimeSpan(24, 0, 0).TotalMinutes);
+
+        value = value.AddMinutes(GetRandomInteger(0, minuteSpan));
+      }
+
+      var stringValue = DateUtil.ToIsoDate(value);
+
+      if(DateTimeFormatHuman)
+        stringValue = value.ToString();
+
+      return new CommandResult(CommandStatus.Success, stringValue);
+    }
+
+    protected int GetRandomInteger(int min, int max)
+    {
+      lock (RandomLock)
+      {
+        return RandomProvider.Next(min, max);
+      }
+    }
+
+    protected double GetRandomDouble()
+    {
+      lock (RandomLock)
+      {
+        return RandomProvider.NextDouble();
+      }
+    }
+
+    public override string Description()
+    {
+      return "Generate random numbers, dates and times";
+    }
+
+    public override void Help(HelpDetails details)
+    {
+      details.AddExample("10");
+      details.AddExample("5 50");
+      details.AddExample("10 -f 2");
+      details.AddExample("-d 2020-12-31");
+      details.AddExample("-d -t 2020-12-31");
+      details.AddExample("-t");
+      details.AddExample("-d -t 2010-01-01 2020-12-31");
+    }
+  }
 }
--- Revolver.Test/Random.cs
+++ Revolver.Test/Random.cs
@@ -5,179 +5,208 @@ using System.Collections.Generic;
 using System.Linq;
 using Sitecore.Shell.Applications.ContentEditor;
 using Cmd = Revolver.Core.Commands;
+using Sitecore;
 
 namespace Revolver.Test
 {
-    [TestFixture]
-    [Category("Random")]
-    public class Random
+  [TestFixture]
+  [Category("Random")]
+  public class Random
+  {
+    [Test]
+    public void FractalWithDate()
     {
-        [Test]
-        public void FractalWithDate()
-        {
-            var cmd = new Cmd.Random
-            {
-                FractalDigits = 2,
-                GenerateDates = true
-            };
+      var cmd = new Cmd.Random
+      {
+        FractalDigits = 2,
+        GenerateDates = true
+      };
 
-            var result = cmd.Run();
+      var result = cmd.Run();
 
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Failure));
-        }
-
-        [Test]
-        public void FractalWithTime()
-        {
-            var cmd = new Cmd.Random
-            {
-                FractalDigits = 2,
-                GenerateDates = true
-            };
-
-            var result = cmd.Run();
-
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Failure));
-        }
-
-        [Test]
-        public void OnlyMaximum()
-        {
-            var cmd = new Cmd.Random
-            {
-                Maximum = "10"
-            };
-            var result = cmd.Run();
-
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
-
-            var num = int.Parse(result.Message);
-
-            Assert.That(num, Is.GreaterThanOrEqualTo(0));
-            Assert.That(num, Is.LessThanOrEqualTo(10));
-        }
-
-        [Test]
-        public void MaximumAndMinimum()
-        {
-            var cmd = new Cmd.Random
-            {
-                Minimum = "50", 
-                Maximum = "5000"
-            };
-            var result = cmd.Run();
-
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
-
-            var num = int.Parse(result.Message);
-
-            Assert.That(num, Is.GreaterThanOrEqualTo(50));
-            Assert.That(num, Is.LessThanOrEqualTo(5000));
-        }
-
-        [Test]
-        public void Fractions()
-        {
-            var cmd = new Cmd.Random
-            {
-                Maximum = "10",
-                FractalDigits = 3
-            };
-            var result = cmd.Run();
-
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
-
-            var num = double.Parse(result.Message);
-
-            Assert.That(num, Is.GreaterThanOrEqualTo(0));
-            Assert.That(num, Is.LessThanOrEqualTo(10));
-
-            var index = result.Message.IndexOf('.');
-            Assert.That(index, Is.EqualTo(result.Message.Length - 4), message: "Incorrect number of decimal digits in " + result.Message);
-        }
-
-        [Test]
-        public void Negative()
-        {
-            var cmd = new Cmd.Random
-            {
-                Minimum = "-20",
-                Maximum = "-10"
-            };
-            var result = cmd.Run();
-
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
-
-            var num = int.Parse(result.Message);
-
-            Assert.That(num, Is.GreaterThanOrEqualTo(-20));
-            Assert.That(num, Is.LessThanOrEqualTo(-10));
-        }
-
-        [Test]
-        public void MultipleInstances()
-        {
-            var commands = new List<Cmd.Random>();
-
-            for (int i = 0; i < 10; i++)
-            {
-                var command = new Cmd.Random
-                {
-                    Maximum = int.MaxValue.ToString()
-                };
-                commands.Add(command);
-            }
-
-            var returnedValues = new List<int>();
-
-            foreach (var command in commands)
-            {
-                var result = command.Run();
-                returnedValues.Add(int.Parse(result.Message));
-            }
-
-            int firstValue = returnedValues.First();
-
-            if (returnedValues.Any(x => x != firstValue))
-            {
-                return;
-            }
-
-            Assert.Fail("Multiple instances of random command return same value.");
-        }
-
-        [Test]
-        public void DateNoRange()
-        {
-            var command = new Cmd.Random
-            {
-                GenerateDates = true
-            };
-
-            var result = command.Run();
-
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
-
-            var generatedDate = System.DateTime.Parse(result.Message);
-        }
-
-        [Test]
-        public void DateWithLimits()
-        {
-            var command = new Cmd.Random
-            {
-                GenerateDates = true,
-                Minimum = "2012-01-01",
-                Maximum = "2012-06-01"
-            };
-
-            var result = command.Run();
-
-            Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
-
-            var generatedDate = System.DateTime.Parse(result.Message);
-            Assert.That(generatedDate, Is.LessThanOrEqualTo(new System.DateTime(2012, 6, 1)));
-            Assert.That(generatedDate, Is.GreaterThanOrEqualTo(new System.DateTime(2012, 1, 1)));
-        }
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Failure));
     }
+
+    [Test]
+    public void FractalWithTime()
+    {
+      var cmd = new Cmd.Random
+      {
+        FractalDigits = 2,
+        GenerateDates = true
+      };
+
+      var result = cmd.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Failure));
+    }
+
+    [Test]
+    public void OnlyMaximum()
+    {
+      var cmd = new Cmd.Random
+      {
+        Maximum = "10"
+      };
+      var result = cmd.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
+
+      var num = int.Parse(result.Message);
+
+      Assert.That(num, Is.GreaterThanOrEqualTo(0));
+      Assert.That(num, Is.LessThanOrEqualTo(10));
+    }
+
+    [Test]
+    public void MaximumAndMinimum()
+    {
+      var cmd = new Cmd.Random
+      {
+        Minimum = "50",
+        Maximum = "5000"
+      };
+      var result = cmd.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
+
+      var num = int.Parse(result.Message);
+
+      Assert.That(num, Is.GreaterThanOrEqualTo(50));
+      Assert.That(num, Is.LessThanOrEqualTo(5000));
+    }
+
+    [Test]
+    public void HumanFriendlyWithoutDateOrTime()
+    {
+      var cmd = new Cmd.Random
+      {
+        DateTimeFormatHuman = true
+      };
+      var result = cmd.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Failure));
+    }
+
+    [Test]
+    public void Fractions()
+    {
+      var cmd = new Cmd.Random
+      {
+        Maximum = "10",
+        FractalDigits = 3
+      };
+      var result = cmd.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
+
+      var num = double.Parse(result.Message);
+
+      Assert.That(num, Is.GreaterThanOrEqualTo(0));
+      Assert.That(num, Is.LessThanOrEqualTo(10));
+
+      var index = result.Message.IndexOf('.');
+      Assert.That(index, Is.EqualTo(result.Message.Length - 4), message: "Incorrect number of decimal digits in " + result.Message);
+    }
+
+    [Test]
+    public void Negative()
+    {
+      var cmd = new Cmd.Random
+      {
+        Minimum = "-20",
+        Maximum = "-10"
+      };
+      var result = cmd.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
+
+      var num = int.Parse(result.Message);
+
+      Assert.That(num, Is.GreaterThanOrEqualTo(-20));
+      Assert.That(num, Is.LessThanOrEqualTo(-10));
+    }
+
+    [Test]
+    public void MultipleInstances()
+    {
+      var commands = new List<Cmd.Random>();
+
+      for (int i = 0; i < 10; i++)
+      {
+        var command = new Cmd.Random
+        {
+          Maximum = int.MaxValue.ToString()
+        };
+        commands.Add(command);
+      }
+
+      var returnedValues = new List<int>();
+
+      foreach (var command in commands)
+      {
+        var result = command.Run();
+        returnedValues.Add(int.Parse(result.Message));
+      }
+
+      int firstValue = returnedValues.First();
+
+      if (returnedValues.Any(x => x != firstValue))
+      {
+        return;
+      }
+
+      Assert.Fail("Multiple instances of random command return same value.");
+    }
+
+    [Test]
+    public void DateNoRange()
+    {
+      var command = new Cmd.Random
+      {
+        GenerateDates = true
+      };
+
+      var result = command.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
+
+      var generatedDate = DateUtil.ParseDateTime(result.Message, System.DateTime.UtcNow);
+    }
+
+    [Test]
+    public void DateNoRangeHumanFriendly()
+    {
+      var command = new Cmd.Random
+      {
+        GenerateDates = true,
+        DateTimeFormatHuman = true
+      };
+
+      var result = command.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
+
+      var generatedDate = System.DateTime.Parse(result.Message);
+    }
+
+    [Test]
+    public void DateWithLimits()
+    {
+      var command = new Cmd.Random
+      {
+        GenerateDates = true,
+        Minimum = "2012-01-01",
+        Maximum = "2012-06-01"
+      };
+
+      var result = command.Run();
+
+      Assert.That(result.Status, Is.EqualTo(CommandStatus.Success));
+
+      var generatedDate = DateUtil.ParseDateTime(result.Message, System.DateTime.UtcNow);
+      Assert.That(generatedDate, Is.LessThanOrEqualTo(new System.DateTime(2012, 6, 1)));
+      Assert.That(generatedDate, Is.GreaterThanOrEqualTo(new System.DateTime(2012, 1, 1)));
+    }
+  }
 }
